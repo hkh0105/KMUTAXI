63523525fa0e8ce281781febedbc77d8a0991ab8
Youngihm <youngihm@gmail.com>
2020-06-04 02:01:30 +0900

    hongdae css

--- KMUTAXI/hong/form.py
+++ KMUTAXI/hong/form.py
@@ -4,4 +4,9 @@ from .models import HongdaeBoardModel
 class HongdaeBoardModelForm(forms.ModelForm):
     class Meta:
         model = HongdaeBoardModel
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'mytitleclass'}),
+            'body': forms.TextInput(attrs={'class': 'mybodyclass'}),
+        }
+
         fields = ['title','body']
